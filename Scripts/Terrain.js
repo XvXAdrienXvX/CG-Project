b6540a14f24b8e0acc3ac314b9ec7af92915debf
Adrien <adrienvadiavaloo@gmail.com>
2023-11-24 10:01:30 -0400

    code refactor

--- Scripts/Terrain.js
+++ Scripts/Terrain.js
@@ -144,43 +144,38 @@ class Terrain {
      * Fill the vertex and buffer arrays 
      */
     generateTriangles() {
-      var deltaX = (this.maxX - this.minX) / this.div;
-      var deltaY = (this.maxY - this.minY) / this.div;
-  
-      for (var i = 0; i <= this.div; i++) {
-        for (var j = 0; j <= this.div; j++) {
-          this.vBuffer.push(this.minX + deltaX * j);
-          this.vBuffer.push(this.minY + deltaY * i);
-          this.vBuffer.push(0);
-  
-          this.nBuffer.push(0);
-          this.nBuffer.push(0);
-          this.nBuffer.push(0);
+      const deltaX = (this.maxX - this.minX) / this.div;
+      const deltaY = (this.maxY - this.minY) / this.div;
+    
+      // Generate vertices
+      for (let i = 0; i <= this.div; i++) {
+        for (let j = 0; j <= this.div; j++) {
+          const x = this.minX + deltaX * j;
+          const y = this.minY + deltaY * i;
+    
+          this.vBuffer.push(x, y, 0);
+          this.nBuffer.push(0, 0, 0);
         }
       }
-  
+    
       this.numVertices = this.vBuffer.length / 3;
       this.updateVertices();
       this.getMinZMaxZ();
-  
-      for (var i = 0; i < this.div; i++) {
-        for (var j = 0; j < this.div; j++) {
-          var vid = i * (this.div + 1) + j;
-          this.fBuffer.push(vid);
-          this.fBuffer.push(vid + 1);
-          this.fBuffer.push(vid + this.div + 1);
-  
-          this.fBuffer.push(vid + 1);
-          this.fBuffer.push(vid + 1 + this.div + 1);
-          this.fBuffer.push(vid + this.div + 1);
+    
+      // Generate triangles
+      for (let i = 0; i < this.div; i++) {
+        for (let j = 0; j < this.div; j++) {
+          const vid = i * (this.div + 1) + j;
+    
+          this.fBuffer.push(vid, vid + 1, vid + this.div + 1);
+          this.fBuffer.push(vid + 1, vid + 1 + this.div + 1, vid + this.div + 1);
         }
       }
-  
+    
       this.numFaces = this.fBuffer.length / 3;
-  
+    
       this.updateNormals();
-  
-    }
+    }    
   
     /**
      * Get minZ and maxZ
@@ -198,94 +193,97 @@ class Terrain {
      * Generate the plane by randomly devide the vertices
      */
     updateVertices() {
-      var iterations = 200;
-      // Adjustment of each iteration
-      var delta = 0.0035;
-  
-      for (var i = 0; i < iterations; i++) {
-        var point = [Math.random() * (this.maxX - this.minX) + this.minX, Math.random() * (this.maxY - this.minY) + this.minY];
-        var n = glMatrix.vec2.create();
-        glMatrix.vec2.random(n);
-        for (var j = 0; j < this.numVertices; j++) {
-          var b = [this.vBuffer[j * 3], this.vBuffer[j * 3 + 1]];
-          if ((b[0] - point[0]) * n[0] + (b[1] - point[1]) * n[1] > 0) {
-            this.vBuffer[j * 3 + 2] += delta;
-          } else {
-            this.vBuffer[j * 3 + 2] -= delta;
-          }
+      const iterations = 200;
+      const delta = 0.0035; // height
+      const randomPoint = () => [Math.random() * (this.maxX - this.minX) + this.minX, Math.random() * (this.maxY - this.minY) + this.minY];
+      const randomDirection = () => glMatrix.vec2.random(glMatrix.vec2.create());
+    
+      for (let i = 0; i < iterations; i++) {
+        const point = randomPoint();
+        const direction = randomDirection();
+    
+        for (let j = 0; j < this.numVertices; j++) {
+          const vertex = [this.vBuffer[j * 3], this.vBuffer[j * 3 + 1]]; // extract x and y coordinates from buffer
+          const dotProduct = (vertex[0] - point[0]) * direction[0] + (vertex[1] - point[1]) * direction[1];
+    
+          this.vBuffer[j * 3 + 2] += dotProduct > 0 ? delta : -delta;
         }
       }
-    }
+    }    
   
     /**
      * Calculate per-vertex normals
      */
     updateNormals() {
-      for (var i = 0; i < this.numFaces; i++) {
-        var vid1 = this.fBuffer[i * 3];
-        var vid2 = this.fBuffer[i * 3 + 1];
-        var vid3 = this.fBuffer[i * 3 + 2];
-  
-        var v1 = [this.vBuffer[vid1 * 3], this.vBuffer[vid1 * 3 + 1], this.vBuffer[vid1 * 3 + 2]];
-        var v2 = [this.vBuffer[vid2 * 3], this.vBuffer[vid2 * 3 + 1], this.vBuffer[vid2 * 3 + 2]];
-        var v3 = [this.vBuffer[vid3 * 3], this.vBuffer[vid3 * 3 + 1], this.vBuffer[vid3 * 3 + 2]];
-        var n1 = [this.nBuffer[vid1 * 3], this.nBuffer[vid1 * 3 + 1], this.nBuffer[vid1 * 3 + 2]];
-        var n2 = [this.nBuffer[vid2 * 3], this.nBuffer[vid2 * 3 + 1], this.nBuffer[vid2 * 3 + 2]];
-        var n3 = [this.nBuffer[vid3 * 3], this.nBuffer[vid3 * 3 + 1], this.nBuffer[vid3 * 3 + 2]];
-  
+      const computeVector = (v1, v2) => {
+        const result = glMatrix.vec3.create();
+        glMatrix.vec3.sub(result, v2, v1);
+        return result;
+      };
+    
+      for (let i = 0; i < this.numFaces; i++) {
+        const vid1 = this.fBuffer[i * 3];
+        const vid2 = this.fBuffer[i * 3 + 1];
+        const vid3 = this.fBuffer[i * 3 + 2];
+    
+        const v1 = this.getVertex(vid1);
+        const v2 = this.getVertex(vid2);
+        const v3 = this.getVertex(vid3);
+    
+        const n1 = this.getNormal(vid1);
+        const n2 = this.getNormal(vid2);
+        const n3 = this.getNormal(vid3);
+    
         // Compute Normal vector
-        var t1 = glMatrix.vec3.create();
-        glMatrix.vec3.sub(t1, v2, v1);
-        var t2 = glMatrix.vec3.create();
-        glMatrix.vec3.sub(t2, v3, v1);
-        var n = glMatrix.vec3.create();
+        const t1 = computeVector(v2, v1);
+        const t2 = computeVector(v3, v1);
+        const n = glMatrix.vec3.create();
         glMatrix.vec3.cross(n, t1, t2);
-  
-        // Add to vertices
+    
+        // Add to normals
         glMatrix.vec3.add(n1, n1, n);
         glMatrix.vec3.add(n2, n2, n);
         glMatrix.vec3.add(n3, n3, n);
-        [this.nBuffer[vid1 * 3], this.nBuffer[vid1 * 3 + 1], this.nBuffer[vid1 * 3 + 2]] = n1;
-        [this.nBuffer[vid2 * 3], this.nBuffer[vid2 * 3 + 1], this.nBuffer[vid2 * 3 + 2]] = n2;
-        [this.nBuffer[vid3 * 3], this.nBuffer[vid3 * 3 + 1], this.nBuffer[vid3 * 3 + 2]] = n3;
+    
+        this.setNormal(vid1, n1);
+        this.setNormal(vid2, n2);
+        this.setNormal(vid3, n3);
       }
-  
+    
       // Normalize
-      for (var i = 0; i < this.numVertices; i++) {
-        var n = [this.nBuffer[i * 3], this.nBuffer[i * 3 + 1], this.nBuffer[i * 3 + 2]];
+      for (let i = 0; i < this.numVertices; i++) {
+        const n = this.getNormal(i);
         glMatrix.vec3.normalize(n, n);
-        [this.nBuffer[i * 3], this.nBuffer[i * 3 + 1], this.nBuffer[i * 3 + 2]] = n;
-      }
-  
-    }
-  
-    /**
-     * Print vertices and triangles to console for debugging
-     */
-    printBuffers() {
-  
-      for (var i = 0; i < this.numVertices; i++) {
-        console.log("v ", this.vBuffer[i * 3], " ",
-          this.vBuffer[i * 3 + 1], " ",
-          this.vBuffer[i * 3 + 2], " ");
-  
-      }
-  
-      for (var i = 0; i < this.numFaces; i++) {
-        console.log("f ", this.fBuffer[i * 3], " ",
-          this.fBuffer[i * 3 + 1], " ",
-          this.fBuffer[i * 3 + 2], " ");
-  
-      }
-  
-      for (var i = 0; i < this.numVertices; i++) {
-        console.log("n ", this.nBuffer[i * 3], " ",
-          this.nBuffer[i * 3 + 1], " ",
-          this.nBuffer[i * 3 + 2], " ");
-  
+        this.setNormal(i, n);
       }
     }
+    
+    // Helper functions for getting and setting vertices and normals
+    getVertex(index) {
+      return [
+        this.vBuffer[index * 3],
+        this.vBuffer[index * 3 + 1],
+        this.vBuffer[index * 3 + 2]
+      ];
+    }
+    
+    getNormal(index) {
+      return [
+        this.nBuffer[index * 3],
+        this.nBuffer[index * 3 + 1],
+        this.nBuffer[index * 3 + 2]
+      ];
+    }
+    
+    setNormal(index, normal) {
+      [
+        this.nBuffer[index * 3],
+        this.nBuffer[index * 3 + 1],
+        this.nBuffer[index * 3 + 2]
+      ] = normal;
+    }    
   
+
     /**
      * Generates line values from faces in faceArray
      * to enable wireframe rendering
